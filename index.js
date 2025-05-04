38e2cb15eceb169fa645b5b84e21b8ce09d742df
erzabytyci <151072186+erzabytyci@users.noreply.github.com>
2025-05-04 22:33:17 +0300

    3.7 Phonebook backend step 7

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 let people = [
   { id: "1", name: "Arto Hellas", number: "040-123456" },
