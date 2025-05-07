ccd45fa04b15f2735922a29d7394c79b6392d148
erzabytyci <151072186+erzabytyci@users.noreply.github.com>
2025-05-07 19:06:18 +0300

    3.14 Phonebook databse, step 2

--- index.js
+++ index.js
@@ -16,13 +16,6 @@ morgan.token('body', (req) => {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 
-// let people = [
-//   { id: "1", name: "Arto Hellas", number: "040-123456" },
-//   { id: "2", name: "Ada Lovelace", number: "39-44-5323523" },
-//   { id: "3", name: "Dan Abramov", number: "12-43-234345" },
-//   { id: "4", name: "Mary Poppendieck", number: "39-23-6423122" }
-// ]
-
 app.get('/api/persons', (request, response) => {
      Person.find({}).then(people => {
         response.json(people)
@@ -66,27 +59,33 @@ app.delete('/api/persons/:id', (request, response) => {
 })
 
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
     if(!body.name || !body.number) {
         return response.status(400).json({error: 'name or number is missing'})
     }
 
-    const nameExist = people.some(person => person.name === body.name)
-    if(nameExist) {
-        return response.status(400).json({error: 'name must be unique'})
-    }
+    // const nameExist = people.some(person => person.name === body.name)
+    // if(nameExist) {
+    //     return response.status(400).json({error: 'name must be unique'})
+    // }
 
-    const person = {
+    const person = new Person({
         name: body.name,
         number: body.number,
-        id: Math.floor(Math.random() * 1000000).toString(),
-    }
+        // id: Math.floor(Math.random() * 1000000).toString(),
+    })
 
-    people = people.concat(person)
+    person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
 
-    response.json(person)
+    // people = people.concat(person)
+
+    // response.json(person)
 })
 
 const PORT = process.env.PORT || 3001
